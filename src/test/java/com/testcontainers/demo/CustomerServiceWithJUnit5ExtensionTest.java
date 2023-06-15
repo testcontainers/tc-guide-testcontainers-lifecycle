964465ed904fddcddf366b6533303a7f306c1af3
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-06-15 10:55:16 +0530

    Update test using extension

--- src/test/java/com/testcontainers/demo/CustomerServiceWithJUnit5ExtensionTest.java
+++ src/test/java/com/testcontainers/demo/CustomerServiceWithJUnit5ExtensionTest.java
@@ -1,8 +1,11 @@
 package com.testcontainers.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -20,6 +23,17 @@ class CustomerServiceWithJUnit5ExtensionTest {
     @BeforeEach
     void setUp() {
         customerService = new CustomerService(postgres.getJdbcUrl(), postgres.getUsername(), postgres.getPassword());
+        customerService.deleteAllCustomers();
+    }
+
+    @Test
+    void shouldCreateCustomer() {
+        customerService.createCustomer(new Customer(1L, "George"));
+
+        Optional<Customer> customer = customerService.getCustomer(1L);
+        assertTrue(customer.isPresent());
+        assertEquals(1L, customer.get().id());
+        assertEquals("George", customer.get().name());
     }
 
     @Test
