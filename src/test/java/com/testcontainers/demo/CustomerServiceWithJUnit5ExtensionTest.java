947c7418d522935b6d8c852c38995493774a4578
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-06-15 16:15:46 +0530

    Add more tests

--- src/test/java/com/testcontainers/demo/CustomerServiceWithJUnit5ExtensionTest.java
+++ src/test/java/com/testcontainers/demo/CustomerServiceWithJUnit5ExtensionTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
